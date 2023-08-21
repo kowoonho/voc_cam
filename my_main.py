483ff9e43aa2e834b55bd8cd424b7abce0494742
kowoonho <heesuk9898@gmail.com>
2023-08-21 12:24:30 +0900

    cvml

--- my_main.py
+++ my_main.py
@@ -91,13 +91,13 @@ def main_process():
         
 
         # step
-        parser.add_argument("--train_cam_pass", default=True)
+        parser.add_argument("--train_cam_pass", default=False)
         parser.add_argument("--make_cam_pass", default=True)
         parser.add_argument("--eval_cam_pass", default=False)
         parser.add_argument("--cam_to_ir_label_pass", default=True)
-        parser.add_argument("--train_irn_pass", default=True)
+        parser.add_argument("--train_irn_pass", default=False)
         parser.add_argument("--make_sem_seg_pass", default=True)
-        parser.add_argument("--eval_sem_seg_pass", default=False)
+        parser.add_argument("--eval_sem_seg_pass", default=True)
         
         
         parser.add_argument("--crop", default=False)
